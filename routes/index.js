d42f8ed7c5d1bfe632b642f2d9dd0e0c65d735c8
shjeon-datahive <tmdgns893758@gmail.com>
2021-09-01 16:26:04 +0900

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 var express = require("express");
 var got = require("got");
+var url = require("url")
+var http = require(`http`);
 var uuid = require("uuid").v4;
 
 var router = express.Router();
 
-var secretKey = "test_ak_ZORzdMaqN3wQd5k6ygr5AkYXQGwy";
+var secretKey = "test_sk_aBX7zk2yd8yLP949jwLrx9POLqKQ";
+function get_query() { var url = document.location.href; var qs = url.substring(url.indexOf('?') + 1).split('&'); for (var i = 0, result = {}; i < qs.length; i++) { qs[i] = qs[i].split('='); result[qs[i][0]] = decodeURIComponent(qs[i][1]); } return result; }
+
 
 router.get("/", function (req, res) {
   res.render("index", {
@@ -42,6 +46,71 @@ router.get("/success", function (req, res) {
         `/fail?code=${error.response?.body?.code}&message=${error.response?.body?.message}`
       );
     });
+});
+
+router.get("/billingkey", function (req, res) {
+
+  got
+    .post('https://api.tosspayments.com/v1/billing/authorizations/' + req.query.authKey, {
+      headers: {
+        Authorization:
+          "Basic " + Buffer.from(secretKey + ":").toString("base64"),
+        "Content-Type": "application/json",
+      },
+      json: {
+        customerKey: req.query.customerKey,
+      },
+      responseType: "json",
+    })
+    .then(function (response) {
+      console.log(response.body);
+      // TODO: 구매 완료 비즈니스 로직 구현
+      res.render("billingkey", {
+        title: "성공적으로 구매했습니다",
+        billkey: response.body.billingKey,
+        customerKey: response.body.customerKey,
+        secretKey: "Basic " + Buffer.from(secretKey + ":").toString("base64"),
+      })
+    })
+    .catch(function (error) {
+      res.redirect(
+        `/fail?code=${error.response?.body?.code}&message=${error.response?.body?.message}`
+      );
+    });
+})
+
+router.get("/billingbuy", function (req, res) {
+  got
+    .post('https://api.tosspayments.com/v1/billing/' + req.query.billingkey, {
+      headers: {
+        Authorization:
+          "Basic " + Buffer.from(secretKey + ":").toString("base64"),
+        "Content-Type": "application/json",
+      },
+      json: {
+        customerKey: req.query.customerKey,
+        amount: 100,
+        orderId: req.query.orderid,
+        orderName: "달숲정기구독"
+      },
+      responseType: "json",
+    })
+    .then(function (response) {
+      console.log(response.body);
+      // TODO: 구매 완료 비즈니스 로직 구현
+      res.render("billingbuy", {
+        title: "정기구독완료",
+        totalAmount: response.body.totalAmount
+      });
+    })
+    .catch(function (error) {
+      res.redirect(
+        `/fail?code=${error.response?.body?.code}&message=${error.response?.body?.message}`
+      );
+    });
+
+
+
 });
 
 router.get("/fail", function (req, res) {
